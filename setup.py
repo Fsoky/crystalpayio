3ba1e7e1972f22b585f9cfb4747c4db9859a2e2d
Fsoky <cyberuest0x12@gmail.com>
2023-10-19 16:41:55 +0600

    1.0.0

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+from io import open
+
+
+def read(filename):
+   with open(filename, "r", encoding="utf-8") as file:
+      return file.read()
+
+
+setup(
+   name="crystalpayio",
+   version="1.0.0",
+   description="Asynchronous wrapper for CrystalPay API",
+   long_description=read("README.md"),
+   long_description_content_type="text/markdown",
+   author="Fsoky",
+   author_email="cyberuest0x12@gmail.com",
+   url="https://github.com/Fsoky/aiocrystalpay",
+   keywords="api crystalpay asyncio crypto",
+   packages=find_packages()
+)
